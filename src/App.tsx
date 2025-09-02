22f920b6d1d29bde9a8dd896d0e43e0481b6327e
kurri <tomikbez@gmail.com>
2025-09-02 14:11:54 +0200

    Nastavovanie

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,10 @@ import DashboardPage from './pages/dashboard';
 import AdminCategoriesPage from './pages/AdminCategoriesPage';
 
 export default function App() {
-    const handleLogin = () => {
-        console.log("Login successful");
-    };
-
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
             <Route path="/AdminCategoriesPage" element={<AdminCategoriesPage />} />
         </Routes>
